2f72cd556aaf9b299f0408ee16354b8d21497d89
Print-TesteServer <matheusd760@gmail.com>
2022-09-18 15:59:04 -0300

    Initial commit

---
+++ app/src/main/java/com/matheus/alcoolvsgasolina/MainActivity.java
@@ -0,0 +1,44 @@
+package com.matheus.alcoolvsgasolina;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText editPrecoAlcool;
+    private EditText editPrecoGasolina;
+    private TextView textResultado;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editPrecoAlcool = findViewById(R.id.editAlcool);
+        editPrecoGasolina = findViewById(R.id.editGasolina);
+        textResultado = findViewById(R.id.textResultado);
+
+    }
+    public void calcularPreco(View view){
+        Double precoAlcool = Double.parseDouble(editPrecoAlcool.getText().toString());
+        Double precoGasolina = Double.parseDouble(editPrecoGasolina.getText().toString());
+
+        /* Faz calculo (precoAlcool/ precoGasolina)
+            Se resultado >= 0,85 melhor ultilizar gasolina
+            Senão melhor ultilizar Alcool
+         */
+        Double resultado = precoAlcool/precoGasolina;
+
+        if(resultado>=0.85){
+            textResultado.setText("Melhor ultilizar GASOLINA!");
+        }
+        else{
+            textResultado.setText("Melhor ultilizar ÁLCOOL!");
+        }
+    }
+}
